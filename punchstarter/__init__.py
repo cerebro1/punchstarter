e949f3b6b99866b946aba95e0b69bc253ef847a7
cerebro1 <srivastava.sonali1@gmail.com>
2016-03-11 12:26:16 +0530

    adding folder

---
+++ punchstarter/__init__.py
@@ -0,0 +1,11 @@
+__author__ = 'sonali'
+
+from flask import Flask, render_template
+from flask.ext.script import Manager
+
+app = Flask(__name__)
+manager = Manager(app)
+
+@app.route("/")
+def hello():
+    return render_template("index.html")
