eed00c58fd68c79615ca10aac6979d47bc8e805d
cerebro1 <srivastava.sonali1@gmail.com>
2016-03-11 14:46:41 +0530

    add project model. Project model stores information of project

--- punchstarter/models.py
+++ punchstarter/models.py
@@ -5,4 +5,11 @@ class Member(db.Model):
     first_name = db.Column(db.String(100))
     last_name = db.Column(db.String(100))
     date_of_birth = db.Column(db.DateTime)
+
+class Project(db.Model):
+    name = db.Column(db.String(100))
+    description = db.Column(db.Text)
+    goal_amount = db.Column(db.Integer)
+    date_of_start = db.Column(db.DateTime)
+    date_of_end = db.Column(db.DateTime)
     
