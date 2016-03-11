9a819bb54ff0a0c983aa394860ca1b745d21ad97
cerebro1 <srivastava.sonali1@gmail.com>
2016-03-11 15:14:40 +0530

    add primary key to each model

--- punchstarter/models.py
+++ punchstarter/models.py
@@ -2,11 +2,13 @@ __author__ = 'sonali'
 from punchstarter import db
 
 class Member(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
     first_name = db.Column(db.String(100))
     last_name = db.Column(db.String(100))
     date_of_birth = db.Column(db.DateTime)
 
 class Project(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(100))
     description = db.Column(db.Text)
     goal_amount = db.Column(db.Integer)
@@ -14,5 +16,6 @@ class Project(db.Model):
     date_of_end = db.Column(db.DateTime)
 
 class Pledge(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
     amount = db.Column(db.Integer)
     time_created = db.Column(db.DateTime)
