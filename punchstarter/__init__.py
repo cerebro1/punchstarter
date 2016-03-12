d3671f9c4d1cc2871a75393290c960ce5a42c4f6
cerebro1 <srivastava.sonali1@gmail.com>
2016-03-13 02:33:02 +0530

    creating project creation view

--- punchstarter/__init__.py
+++ punchstarter/__init__.py
@@ -1,10 +1,10 @@
 __author__ = 'sonali'
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask.ext.script import Manager
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.migrate import Migrate, MigrateCommand
-
+import datetime
 app = Flask(__name__)
 manager = Manager(app)
 
@@ -19,6 +19,25 @@ from punchstarter.models import *
 def hello():
     return render_template("index.html")
 
-@app.route("/projects/create/")
+@app.route("/projects/create/", methods = ['GET', 'POST'])
 def create():
-    return render_template("create.html")
+    if request.method == 'GET':
+        return render_template("create.html")
+    if request.method == 'POST':
+        time_start = request.form.get("start_date")
+        time_start = datetime.datetime.strptime(time_start,"%Y-%m-%d")
+        time_end = request.form.get("end_date")
+        time_end = datetime.datetime.strptime(time_end,"%Y-%m-%d")
+        new_project = Project(
+            member_id = 1,
+            name = request.form.get("project_name"),
+            description = request.form.get("description"),
+            goal_amount = request.form.get("goal_amount"),
+            date_of_start = time_start,
+            date_of_end = time_end
+        )
+
+        db.session.add(new_project)
+        db.session.commit()
+
+        return redirect(url_for('create'))
