49589172652f802dd96f4760f69882c05d32dd16
cerebro1 <srivastava.sonali1@gmail.com>
2016-03-12 12:12:24 +0530

    updating __init__.py

--- punchstarter/__init__.py
+++ punchstarter/__init__.py
@@ -2,10 +2,15 @@ __author__ = 'sonali'
 
 from flask import Flask, render_template
 from flask.ext.script import Manager
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
 manager = Manager(app)
 
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 @app.route("/")
 def hello():
     return render_template("index.html")
